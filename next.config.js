3e893f7a2d3c6aae4c0b0fc398f81f019e6fe6cf
shahid basheer <muhammadshahid4127@gmail.com>
2023-09-18 02:30:33 +0530

    added webhook key

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     NEXTAUTH_SECRET:"coding",
     STRIPE_PUBLIC_KEY: "pk_test_51Np8LVSJO1ZZV7ezQo86PEQJD0w2wWi5BtnjKNUMyjz5yrJDrNzks8tWpPpThKjdfieeikQuGYqOcHFQP8gTPeH800NUaetTSO",
     STRIPE_PRIVATE_KEY: "sk_test_51Np8LVSJO1ZZV7ezXztwxc33RRu9bGfs1KkXJZnYzG1dmUCchb7Uow8z6FfskK6gf5CgivgZmoLOKm2lH5Fb86Cz00RGVo8Kgo",
-    STRIPE_WEBHOOK_SECRET:"whsec_47b003f38b425fde905b48e0d9d7d15b6377a2f3054857a08364647bdd17fa20",
+    STRIPE_WEBHOOK_SECRET:"we_1NrRxYSJO1ZZV7ezwatZwDLH",
   },
   images:{
     domains:['res.cloudinary.com','encrypted-tbn0.gstatic.com']
